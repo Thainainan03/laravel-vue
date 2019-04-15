6fff9bf886a82fbde18c41fe55e712379886de67
Zerojame <toypoyomn03@outlook.com>
2019-04-15 10:14:48 +0700

    make it better

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -3,10 +3,10 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Example Component</div>
-
+                    <div class="card-header">Example Component dasd dsaddasd flsjdflj</div>
+dasdasda     sa
                     <div class="card-body">
-                        I'm an example component.
+                        I'm an example component. dasd alskdalsj adflgjadl dalsdmlaksdj
                     </div>
                 </div>
             </div>
@@ -17,7 +17,11 @@
 <script>
     export default {
         mounted() {
-            console.log('Component mounted.')
+            console.log('Component mounted. dasd')
         }
     }
 </script>
+
+
+
+
